076b7cfbc1fc92a4fd7743eabfc5fcae0763025a
Kris Casper <sw.darkwind@gmail.com>
2020-09-29 15:42:35 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+  let res = 0
+  if(Array.isArray(array) && array.length > 0) array.forEach(d => res = Math.min(res, d))
+  return res
 }
 
 exports.max = function max (array) {
-  return 0;
+  let res = 0
+  if(Array.isArray(array) && array.length > 0) array.forEach(d => res = Math.max(res, d))
+  return res
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!Array.isArray(array) || array.length < 1) return 0
+  let summ = 0
+  array.forEach(d => summ += d)
+  return summ / array.length
 }
